b800b309b2eecbee1f63c443c330a3cb1ea6c2ad
Mitasiek <mitasiek@gmail.com>
2023-03-17 20:30:25 +0100

    contact.html map

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-$('.nav-side .nav-toggle').on('click', function(e){
-    e.preventDefault();
-    $(this).parent().toggleClass('nav-open');
-});
+let mapOptions = {
+    center:[51.958, 9.141],
+    zoom:10
+}
+
+
+let map = new L.map('map' , mapOptions);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let marker = new L.Marker([51.958, 9.141]);
+marker.addTo(map);
